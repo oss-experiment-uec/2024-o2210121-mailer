c1b7b248e31cd0da7ad8625287cb036130ab4685
ReiyaOhnishi <o2210121@gl.cc.uec.ac.jp>
2024-12-23 02:31:26 +0900

    change testfile(in progress)

---
+++ testfile.py
@@ -0,0 +1,19 @@
+# testfile
+from marrow.mailer import Mailer, Message
+
+mailer = Mailer(dict(
+        transport = dict(
+                use = 'smtp',
+                host = 'smtp.gmail.com', 
+                port=587,
+                tls='starttls',
+                username='ruri9752@gmail.com',
+                password='Mikusururi1024')))
+mailer.start()
+
+message = Message(author="ruri9572@gmail.com", to="iferi0103@gmail.com")
+message.subject = "Testing Marrow Mailer"
+message.plain = "This is a test."
+mailer.send(message)
+
+mailer.stop()
